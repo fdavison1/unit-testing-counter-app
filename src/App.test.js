44a260b4684c33f81f8194c6dc3a795a501bbab6
fdavison1 <fred.noreply@gmail.com>
2019-12-04 09:51:30 -0700

    initial set up setupTests.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
+import './setupTests'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('App Component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />)
+    const text = wrapper.find('p').text()
+    expect(text).toEqual('Count: 0')
+  })
+})
